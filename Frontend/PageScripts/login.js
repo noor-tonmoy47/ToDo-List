a5f16b13b8c2e7144ed51660f29901bc15fa4bf0
tonmoy-swe-043 <shamsnoor422@gmail.com>
2023-07-24 04:26:59 +0600

    Connected login api with the login form

--- Frontend/PageScripts/login.js
+++ Frontend/PageScripts/login.js
@@ -10,3 +10,38 @@ function toggleForm(formId) {
       signupForm.style.display = 'block';
     }
   }
+
+  //Login
+  
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // POST request to the server API to handle login
+    fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+        
+    })
+    .then(response => response.json())
+    .then(data => {
+        // If login is successful, data will contain the JWT token
+        const token = data.token;
+        console.log(token);
+        // Saving the token to local storage or a cookie for subsequent requests
+        localStorage.setItem('token', token);
+        localStorage.setItem('username', username);
+        // Redirecting to the todo list page
+        window.location.href = './Todo.html'; 
+    })
+    .catch(error => {
+        
+        console.error('Login error:', error);
+        alert('Login failed. Please check your username and password.');
+    });
+});
