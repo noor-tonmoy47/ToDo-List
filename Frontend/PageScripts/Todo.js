4a19a8bf6fd0c1919dbce5c5293856a02a03f4b6
tonmoy-swe-043 <shamsnoor422@gmail.com>
2023-07-24 04:35:53 +0600

    Connected the createTask api with the frontend

--- Frontend/PageScripts/Todo.js
+++ Frontend/PageScripts/Todo.js
@@ -1,6 +1,8 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+const baseUrl = 'http://localhost:3000'; 
+
 function addTask(){
     if(inputBox.value === ''){
         alert("You must write something");
@@ -9,14 +11,41 @@ function addTask(){
         let li= document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-
+        li.id = Date.now().toString();
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        addListToDB(li.id, inputBox.value);
     }
     inputBox.value ='';
 }
 
+function addListToDB(id, content) {
+    const token = localStorage.getItem('token'); 
+
+    //POST request to the createlist endpoint on your server
+    fetch(baseUrl + createListEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token: token, itemName: content, id: id })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to create the task. Please try again later.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle the response data (e.g., show success message or refresh the task list)
+            console.log(data.message);
+        })
+        .catch(error => {
+            console.error(error.message);
+            alert(error.message);
+        });
+}
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
