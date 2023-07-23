cdfc8643e29c6b1940f97f78597f6d9681059f98
tonmoy-swe-043 <shamsnoor422@gmail.com>
2023-07-24 04:37:14 +0600

    Connected the deleteTask api with the frontend

--- Frontend/PageScripts/Todo.js
+++ Frontend/PageScripts/Todo.js
@@ -52,6 +52,36 @@ listContainer.addEventListener("click", function(e){
         e.target.classList.toggle("checked");
     }
     else if(e.target.tagName === "SPAN"){
+        deleteFromDB(e.target.parentElement.id);
         e.target.parentElement.remove();
     }
 }, false);
+
+function deleteFromDB(id){
+    const token = localStorage.getItem('token'); // Assuming the JWT token is stored in localStorage
+
+    // Make a POST request to the createlist endpoint on your server
+    fetch(baseUrl + "/api/deleteList", {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token: token, itemId: id })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to create the task. Please try again later.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle the response data (e.g., show success message or refresh the task list)
+            alert(data.message);
+            // You can refresh the task list here or update it with the new task
+        })
+        .catch(error => {
+            // Handle errors (e.g., display the error message to the user)
+            console.error(error.message);
+            alert(error.message);
+        });
+}
