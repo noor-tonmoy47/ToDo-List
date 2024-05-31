51a38dd52d92ce4864ac858eb235f4917b4b86e2
noor-tonmoy47 <noortonmoy47@gmail.com>
2024-05-31 10:58:45 +0600

    delete tasks

--- v2-docker/controller/tasks.js
+++ v2-docker/controller/tasks.js
@@ -62,29 +62,48 @@ const createTask = (req, res) =>{
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
 
-        // Insert the new list into the database
-        const insertListSql = 'INSERT INTO lists (username, content) VALUES (?, ?)';
+        // Avoiding duplicate tasks
 
-        db.query(insertListSql, [decodedToken.username, task], (err) => {
-            if (err) {
+        const checkTask = 'SELECT * FROM lists WHERE username = ? AND content = ?';
+
+        db.query(checkTask, [decodedToken.username, task], (err, results)=>{
+            if(err){
                 return res.status(500).json({ 
                     status: "failed",
                     error: 'Internal server error' 
                 });
             }
+            if(results.length > 0){
+                return res.status(409).json({
+                    status: "Conflict",
+                    message: "This task already exists"
+                });
+            }
 
-            return res.status(201).json({ 
-                status: 'success',
-                message: 'Task created successfully.' 
+            // Inserting new task
+
+            const insertListSql = 'INSERT INTO lists (username, content) VALUES (?, ?)';
+    
+            db.query(insertListSql, [decodedToken.username, task], (err) => {
+                if (err) {
+                    return res.status(500).json({ 
+                        status: "failed",
+                        error: 'Internal server error' 
+                    });
+                }
+    
+                return res.status(201).json({ 
+                    status: 'success',
+                    message: 'Task created successfully.' 
+                });
             });
-        });
+        })
     } catch (err) {
         // Handle JWT verification error (invalid token)
         console.error('JWT verification error:', err.message);
         return res.status(401).json({ 
             status: 'failed',
             message: 'Unauthorized' 
-        
         });   
     }
 }
@@ -97,7 +116,64 @@ const updateTask = (req, res) =>{
 
 const deleteTask = (req, res) =>{
     
-    res.status(204).json({message: "deleting tasks"});
-}
+    const { token, task } = req.body;
+
+    // Check if the token and listId are provided
+    if (!token || !task) {
+        return res.status(400).json({ 
+            status: 'Bad Request',
+            message: 'Token and task are required'
+        });    
+    }
+
+    try {
+        // Verify the token to get the user information
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const username = decodedToken.username;
+
+
+        // Check if the list with the given listId belongs to the user
+        const checkListSql = 'SELECT content FROM lists WHERE content = ? AND username = ?';
+
+        db.query(checkListSql, [task, username], (err, results) => {
+            if (err) {
+                return res.status(500).json({ 
+                    status: "failed",
+                    error: "Internal server error"
+                });            
+            }
+
+            // If the list with the given listId doesn't exist or doesn't belong to the user
+            if (results.length === 0) {
+                return res.status(404).json({ 
+                    status: "Not Found",
+                    message: "Task Not Found" 
+                });            
+            }
+
+            // Delete the list from the database
+            const deleteListSql = 'DELETE FROM lists WHERE content = ? AND username = ?';
+
+            db.query(deleteListSql, [task, username], (err, results) => {
+                if (err) {
+                    return res.status(500).json({ 
+                        status: "failed",
+                        error: "Internal server error"
+                    });              
+                }
+
+                return res.status(200).json({ 
+                    status: "success",
+                    message: "Task deleted" 
+                });
+            });
+        });
+    } catch (err) {
+        // Handle JWT verification error (invalid token)
+        return res.status(401).json({ 
+            status: "failed",
+            message: "Unauthorized"
+        }); 
+    }}
 
 module.exports = {all, getAlltasks , createTask, updateTask, deleteTask};
